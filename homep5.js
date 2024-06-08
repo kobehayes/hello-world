00c97fec8361e9f58863c04a2b081afcc08b69f3
Kobe <54520589+kobehayes@users.noreply.github.com>
2024-06-08 17:13:08 -0500

    using p5 for homepage- unfinished

--- homep5.js
+++ homep5.js
@@ -1,73 +1,41 @@
+let images = [];
+let currentIndex = 0;
+let nextIndex = 1;
+let lerpAmount = 0;
 
-
-// this class describes the properties of a single particle.
-class Particle {
-// setting the co-ordinates, radius and the
-// speed of a particle in both the co-ordinates axes.
-  constructor(){
-    this.x = random(0,width);
-    this.y = random(0,height);
-    this.r = random(5,20);
-    this.xSpeed = random(0,0.5);
-    this.ySpeed = random(-0.5,0);
-  }
-
-// creation of a particle.
-  createParticle() {
-    noStroke();
-    fill('rgba(200,169,169)');
-    circle(this.x,this.y,this.r);
-  }
-
-// setting the particle in motion.
-  moveParticle() {
-    if(this.x < 0 || this.x > width)
-      this.xSpeed*=-1;
-    if(this.y < 0 || this.y > height)
-      this.ySpeed*=-1;
-    this.x+=this.xSpeed;
-    this.y+=this.ySpeed;
-  }
-
-// this function creates the connections(lines)
-// between particles which are less than a certain distance apart
-  joinParticles(particles) {
-    particles.forEach(element =>{
-      let dis = dist(this.x,this.y,element.x,element.y);
-      if(dis<85) {
-        stroke('rgba(255,255,255,0.04)');
-        line(this.x,this.y,element.x,element.y);
-      }
-    });
-  }
+function preload() {
+  images[0] = loadImage('aux-imgs/aux2-6.jpg');
+  images[1] = loadImage('photography/pho3.jpeg');
+  // Load more images as needed
 }
 
-// an array to add multiple particles
-let particles = [];
-let cnv;
 function setup() {
-      pixelDensity(1);
-     background(0, 0, 0);
-   cnv = createCanvas(windowWidth, 500);
-    cnv.style('position', 'relative');
-    cnv.style('z-index', '-1');
-  for(let i = 0;i<20;i++){
-    particles.push(new Particle());
-  }
+  let canvas = createCanvas(1500, 600);
+  canvas.parent('canvasContainer');
+  frameRate(30);
 }
-
-
-// function windowResized() {
-//     resizeCanvas(500, 600);
-// }
 
 function draw() {
-     pixelDensity(1);
-    background(0, 0, 0);
-    for(let i = 0;i<particles.length;i++) {
-        particles[i].createParticle();
-        particles[i].moveParticle();
-        particles[i].joinParticles(particles.slice(i));
-    }
-}
+  background(255);
 
+  // Calculate the x-positions of the current and next images
+  let xCurrent = lerp(width, -width, lerpAmount);
+  let xNext = lerp(2 * width, 0, lerpAmount);
+
+  // Draw the current and next images
+  image(images[currentIndex], xCurrent, 0, width, height);
+  image(images[nextIndex], xNext, 0, width, height);
+
+  // Increase the lerp amount
+  lerpAmount += 0.02;
+
+  // If the transition is complete
+  if (lerpAmount >= 1) {
+    // Reset the lerp amount
+    lerpAmount = 0;
+
+    // Move to the next image
+    currentIndex = nextIndex;
+    nextIndex = (nextIndex + 1) % images.length;
+  }
+}
